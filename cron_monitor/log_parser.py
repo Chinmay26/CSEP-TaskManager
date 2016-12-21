cdf3f293fb5ffde110639214606d3f62b09734ba
Chinmay Naik <chin.naik26@gmail.com>
2016-12-20 16:58:08 -0800

    Add filter for log_files based on timestamp + Fix filter if start_time and end_time are of different month/year

--- cron_monitor/log_parser.py
+++ cron_monitor/log_parser.py
@@ -1,7 +1,8 @@
-import datetime
 import subprocess
 import io
+import re
 from db_client import DB_Client
+from datetime import datetime,timedelta
 
 FILE_SEPARATOR = '/'
 MONTH_LOG_SEPARATOR = '_'
@@ -12,8 +13,8 @@ class LogParser:
   '''
   def __init__(self, start_time, end_time):
     self.cron_job_meta = {}
-    db = DB_Client()
-    results = db.execute_sql_command('select * from jobs')
+    self.db = DB_Client()
+    results = self.db.execute_sql_command('select * from jobs')
     for job in results:
       self.cron_job_meta[job['job_name']] = job
     #print('job_meta', self.cron_job_meta)
@@ -40,29 +41,49 @@ class LogParser:
        For a given job and date range, get a month-wise dict of cron log file to be grepped
        {'2016-10': '/home/csep/operations/dispatcher/logs/2016_10/dailyANSS1985_2016-10-*'}
     '''
-    log_file_dict = {}
     job_metadata = self.cron_job_meta[job_name]
     parent_dir = job_metadata['base_path'] + job_metadata['log_dir']
 
     #This will be the list of file paths to search
     file_dict = {}
-    
+    month_directory = []
     #since logs are stored month-wise
     if start_time.year == end_time.year :
       if (start_time.month == end_time.month):
-        month_directory = FILE_SEPARATOR + str(start_time.year) + MONTH_LOG_SEPARATOR + str(start_time.month)
-        parent_dir += month_directory
-        file_meta_path = parent_dir + FILE_SEPARATOR + job_metadata['file_base_name'] + str(start_time.year) + \
-                         LOG_FILE_DATE_SEPARATOR + str(start_time.month) + LOG_FILE_DATE_SEPARATOR
-        month = str(start_time.year) + LOG_FILE_DATE_SEPARATOR + str(start_time.month)
-        file_dict[month] = file_meta_path
+        month_directory.append(start_time)
       else:
-        #TO-DO
-        pass
-    else:
-      pass
-      #TO-DO
+        #Add all timestamps from start_time to end_time
+        #Get all first days of the month from start_time and end_time
+        st_first_day = start_time.replace(day=1)
+        et_first_day = end_time.replace(day=1)
+        temp_date = et_first_day
 
+        while temp_date != st_first_day:
+          month_directory.append(temp_date)
+          temp_date = temp_date - timedelta(days=1)
+          temp_date = temp_date.replace(day=1)
+
+        month_directory.append(temp_date)
+    else:
+      #Add all timestamps from start_time to end_time
+      #Get all first days of the month from start_time and end_time
+      st_first_day = start_time.replace(day=1)
+      et_first_day = end_time.replace(day=1)
+      temp_date = et_first_day
+
+      while temp_date != st_first_day:
+        month_directory.append(temp_date)
+        temp_date = temp_date - timedelta(days=1)
+        temp_date = temp_date.replace(day=1)
+
+      month_directory.append(temp_date)
+
+    for m in month_directory:
+      parent_dir += (FILE_SEPARATOR + str(m.year) + MONTH_LOG_SEPARATOR + str(m.month))
+      file_meta_path = parent_dir + FILE_SEPARATOR + job_metadata['file_base_name'] + str(m.year) + \
+                       LOG_FILE_DATE_SEPARATOR + str(m.month) + LOG_FILE_DATE_SEPARATOR
+      month = str(m.year) + LOG_FILE_DATE_SEPARATOR + str(m.month)
+      file_dict[month] = file_meta_path
     return file_dict
 
   def get_log_files(self, dir_list):
@@ -82,13 +103,12 @@ class LogParser:
     return log_files
 
   def get_db_entries(self, job_data, start_time, end_time):
-    db = DB_Client()
     command = "select log_file_path from job_history where job_id = %s and start_time > %s and end_time < %s"
     st = start_time.strftime('%Y-%m-%d %H:%M:%S')
     et = end_time.strftime('%Y-%m-%d %H:%M:%S')
     query_t = (job_data['id'] ,st, et)
 
-    results = db.execute_sql_command(command, query_t)
+    results = self.db.execute_sql_command(command, query_t)
     db_files = []
     for lg in results:
       db_files.append(lg['log_file_path'])
@@ -99,12 +119,29 @@ class LogParser:
        Assumes start_time and end_time are date time objects
        {'2016-10-21': [log_file_1, log_file_2, log_file_3], '2016-10-22': [log_file_1]}
     '''
-    meta_month_dict = self.get_log_paths(job_name, start_time, end_time)
-    meta_month_list = list(meta_month_dict.values())
-    #print('meta_month_list', meta_month_list)
+    lp = self.get_log_paths(job_name, start_time, end_time)
+    lpv = list(lp.values())
+    #print('meta_month_list', lpv)
     #TO-DO Filter files from start date to end date
-    log_files = self.get_log_files(meta_month_list)
+    log_files = self.get_log_files(lpv)
     #print('log_files11', log_files)
+    return log_files
+
+  def filter_logs_by_time_stamp(self, job_name, logs, start_time, end_time):
+    '''
+       Given log files of the month, filter files based on start time and end time
+    '''
+    job_metadata = self.cron_job_meta[job_name]
+    base_name = job_metadata['file_base_name']
+    log_files = []
+    for file_path in logs:
+      pos = file_path.find(base_name)
+      if pos != -1:
+        ts = file_path[pos+len(base_name):]
+        #ts = '2016-12-9-001001'
+        dt = datetime.strptime(ts, "%Y-%m-%d-%H%M%S")
+        if start_time <= dt and dt <= end_time:
+          log_files.append(file_path)
     return log_files
 
   def get_job_ids(self, job_data):
@@ -121,13 +158,12 @@ class LogParser:
     return proc
 
   def create_job_history_db_entries(self, log_details):
-    db = DB_Client()
     command = 'insert into job_history(job_id,status,start_time,end_time,log_file_path) values(%s,%s,%s,%s,%s)'
 
     query_t = (log_details['job_id'],log_details['status'],log_details['start_time'],log_details['end_time'], 
                log_details['file_path'])
     #print('DB CMD', command, query_t)
-    results = db.execute_sql_command(command, query_t)
+    results = self.db.execute_sql_command(command, query_t)
     return results
 
   def get_job_name_from_log_file(self, log):
@@ -136,14 +172,16 @@ class LogParser:
         return job_name
     return None
 
+  #FIX-THIS
+  #Use base-file-name and extract timestamp
   def extract_time_from_file_name(self, file_name):
     start_time_stamp = file_name.split('_')[-1].split('-')
     seconds = start_time_stamp[-1]
     if len(seconds) != 6:
       return None
-    fromatted_seconds_timestamp = seconds[0:2] + ":" + seconds[2:4] + ":" + seconds[4:]
+    formatted_seconds_timestamp = seconds[0:2] + ":" + seconds[2:4] + ":" + seconds[4:]
     t1 = '-'.join(start_time_stamp[:-1])
-    st = t1 + ' ' + fromatted_seconds_timestamp
+    st = t1 + ' ' + formatted_seconds_timestamp
     return st
 
   def create_success_entries(self, log_files):
@@ -205,13 +243,12 @@ class LogParser:
     '''
        Get DB entries for given time range and given jobs
     '''
-    db = DB_Client()
     command = "select * from job_history where job_id in %s and start_time > %s and end_time < %s"
     st = start_time.strftime('%Y-%m-%d %H:%M:%S')
     et = end_time.strftime('%Y-%m-%d %H:%M:%S')
     job_ids = tuple(self.get_job_ids(self.cron_job_meta))
     query_t = (job_ids, st, et)
-    results = db.execute_sql_command(command, query_t)
+    results = self.db.execute_sql_command(command, query_t)
     return results
 
   def parse(self):
@@ -221,7 +258,9 @@ class LogParser:
     remain_files = []
     for job_name, job_details in self.cron_job_meta.items():
       
-      logs = self.get_cron_log_files_for_job(job_name, self.start_time, self.end_time)
+      log_files = self.get_cron_log_files_for_job(job_name, self.start_time, self.end_time)
+      #print('log_files', log_files)
+      logs = self.filter_logs_by_time_stamp(job_name, log_files, self.start_time, self.end_time)
       #print('logs', logs)
       #Get log files from DB
       db_files = self.get_db_entries({'id': job_details['id']}, self.start_time, self.end_time)
